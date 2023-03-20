87d23980c071e84d1386b4e7e435db49ccdffef1
dab1ca <tsvetantsvetanov89@gmail.com>
2023-03-20 09:29:27 +0000

    03/20v1

--- Scripts/web-server-setup.sh
+++ Scripts/web-server-setup.sh
@@ -4,22 +4,24 @@ set USERPASSWORD [lindex $argv 1]
 set SERVERNAME [lindex $argv 2]
 set APPREPO [lindex $argv 3]
 set WEBSERVERPUBLICIP [lindex $argv 4]
+
+set timeout 25
 spawn ssh $SERVERUSER@$WEBSERVERPUBLICIP
-#expect "Are you sure you want to continue connecting ?"
-#send "yes\r"
-expect "assword:"
-send "$USERPASSWORD\r"
-wait
-send "sudo hostnamectl set-hostname $SERVERNAME\r"
-send "sudo apt update\r"
-send "sudo apt install apache2 php php-mysqlnd git\r"
-expect "Do you want to continue?"
-send "Y\r"
-wait
-send "sudo ufw allow 'Apache' && sudo ufw reset\r"
-expect "Proceed with operation"
-send "Y\r"
-send "sudo git clone $APPREPO\r"
-send "sudo cp -v bgapp/web/* /var/www/html\r"
-send "sudo rm /var/www/html/index.html\r"
-send "echo Finished"
+
+expect "yes/no" { 
+	send "yes\r"
+	expect "assword:" { send "$USERPASSWORD\r" }
+	} "assword:" { send "$USERPASSWORD\r" }
+
+expect "$ " { send "sudo hostnamectl set-hostname $SERVERNAME && sudo apt update && sudo apt install apache2 php php-mysqlnd git\r" }
+
+expect "Do you want to continue?" { send "y\r" }
+
+expect "$ " { send "sudo ufw allow 'Apache' && sudo ufw reset\r" } 
+
+expect "Proceed with operation (y|n)?" { send "y\r" }
+
+expect "$ " { send "sudo git clone $APPREPO && sudo cp -v bgapp/web/* /var/www/html && sudo rm /var/www/html/index.html\r" } 
+
+expect "$ " { send "exit\r" }
+expect eof
