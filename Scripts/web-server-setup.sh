4de1263effa80d9d8d22813bbea2607838ac73f4
dab1ca <tsvetantsvetanov89@gmail.com>
2023-03-22 05:52:19 +0000

    03/22v2 - final

--- Scripts/web-server-setup.sh
+++ Scripts/web-server-setup.sh
@@ -4,6 +4,7 @@ set USERPASSWORD [lindex $argv 1]
 set SERVERNAME [lindex $argv 2]
 set APPREPO [lindex $argv 3]
 set WEBSERVERPUBLICIP [lindex $argv 4]
+set DBPASSWORD [lindex $argv 5]
 
 set timeout 600
 spawn ssh $SERVERUSER@$WEBSERVERPUBLICIP
@@ -21,7 +22,11 @@ expect "$ " { send "sudo ufw allow 'Apache' && sudo ufw reset\r" }
 
 expect "Proceed with operation (y|n)?" { send "y\r" }
 
-expect "$ " { send "sudo git clone $APPREPO && sudo cp -v bgapp/web/* /var/www/html && sudo rm /var/www/html/index.html\r" } 
+expect "$ " { send "git clone $APPREPO && sudo cp -v bgapp/web/* /var/www/html && sudo rm /var/www/html/index.html\r" } 
+
+expect "$ " { send "git clone https://github.com/dab1ca/AzureApp.git && sudo expect /home/$SERVERUSER/AzureApp/Scripts/db-server-setup.sh $SERVERUSER $USERPASSWORD $DBPASSWORD db $APPREPO 10.21.0.101" }
+
+expect "$ " { send "sudo rm -r /home/$SERVERUSER/AzureApp"}
 
 expect "$ " { send "exit\r" }
 expect eof
